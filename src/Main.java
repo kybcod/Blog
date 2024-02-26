eea68a7ce57824db946f3e3af3e44afa11aa76c9
kyb <kyb-0215@naver.com>
2024-02-26 10:31:57 +0900

    객체 생성 변경

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         Laptop laptop = new Laptop("Samsung Laptop", -2_000_000, "게임용 노트북");
-        Laptop laptop1 = new GamingLaptop("Samsung Laptop", 2_000_000, "게임용 노트북","graphicsCard");
-        Laptop laptop2 = new DocumentLaptop("LG Laptop", 1_000_000, "문서 작성용 노트북","Document");
+        GamingLaptop glaptop = new GamingLaptop("Samsung Laptop", 2_000_000, "게임용 노트북","graphicsCard");
+        DocumentLaptop dlaptop = new DocumentLaptop("LG Laptop", 1_000_000, "문서 작성용 노트북","Document");
 
-        System.out.println(laptop.getPrice()); //RuntimeException
-        laptop1.bootUp(); // 출력: "GamingLaptop is booting up..."
-        laptop2.bootUp(); // 출력: "DocumentLaptop is booting up..."
+//        System.out.println(laptop.getPrice()); //RuntimeException
+        glaptop.bootUp(); // 출력: "GamingLaptop is booting up..."
+        dlaptop.bootUp(); // 출력: "DocumentLaptop is booting up..."
 
     }
 }
