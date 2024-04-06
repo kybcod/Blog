2a2f98e4ba9bf854b84b19c3b1b827a2eb8170e0
ybk <kyb-0215@naver.com>
2024-04-06 22:06:29 +0900

    람다사용

--- src/codingtest/Programmers/Programmers1.java
+++ src/codingtest/Programmers/Programmers1.java
@@ -4,20 +4,17 @@ import java.util.*;
 
 public class Programmers1 {
     public static void main(String[] args) {
-//        String s = "a234";
-//        String[] str = s.split("");
-//
-//
-//
-//
-//        System.out.println(s.length());
-//
-//        Arrays.sort(str, (x,y)->y.compareTo(x));
-//        System.out.println(Arrays.toString(str));
-//
-//        String answer = String.join("",str);
-//
-//        System.out.println(answer);
+        String s = "a234";
+        String[] str = s.split("");
+
+        System.out.println(s.length());
+
+        Arrays.sort(str, (x,y)->y.compareTo(x));
+        System.out.println(Arrays.toString(str));
+
+        String answer = String.join("",str);
+
+        System.out.println(answer);
 
 
 
